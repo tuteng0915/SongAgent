417f6d578dcc0d247c084608bebd9f484e12eb9a
RedstoneDave <RedstoneGuo@outlook.com>
2025-09-04 12:12:39 +0800

    minor edit

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from assistant import *
 from tools import *
 import json5
 
-prompt = '''You are a player in a role-playing game. Your role is a user of a song generating dialogue agent, you should tell the agent your requirements about the song, but as a user, you should express your needs in a vague and non-professional way as a real human would do. You also shouldn't express all your needs in one reply, instead, do follow-up requests about your requirements when the agent replies you and you feel the song still imperfect. Meanwhile, the user's role is the song agent, it will respond you and generate the lyrics and the tags of the song, you will be able to retrieve them via the "param_getter" tool. When the generating agent asks you about details of the song, you can either make up some information or simply claim that it's irrelevant. When you feel the song is perfect, use the "halt" tool to terminate the process. Following is your requirements:
+prompt = '''You are a player in a role-playing game. Your role is a user of a song generating dialogue agent, you should tell the agent your requirements about the song, but as a user, you should express your needs in a vague and non-professional way as a real human would do. You also shouldn't express all your needs in one reply, instead, do follow-up requests about your requirements when the agent replies you and you feel the song still imperfect. Meanwhile, the user's role is the song agent, it will respond you and generate the lyrics and the tags of the song, you will be able to retrieve them via the "param_getter" tool. When the generating agent asks you about details of the song, you can either make up some information or simply claim that it's irrelevant. When you feel the song is perfect, use the "halt" tool to terminate the process, this is the equivalent of the "generate" button that the song generating agent mentions. Following is your requirements:
 
 {requirements}
 '''
@@ -30,7 +30,7 @@ class GetParam(BaseTool):
 
 @register_tool('halt')
 class Halt(BaseTool):
-    description = 'halt the process.'
+    description = 'halt the process, the equivalent of "generate" button the agent mentions'
     parameters = []
     
     def call(self, params, **kwargs) -> str:
@@ -60,27 +60,31 @@ vd = {
     'preference': ''
 }
 
-messages = [{
+messages_for_u = [{
     'role': 'user',
     'content': 'Please begin requesting the agent.'
 }]
 
+messages_for_a = []
+
 #'''
-for _ in range(10):
-    for response in user.run(messages, var_dict=vd):
+for _ in range(4):
+    for response in user.run(messages_for_u, var_dict=vd):
         ...
-    print(response)
     if vd['halt']:
         break
-    messages.extend(response)
-    for i in messages:
-        i['role'] = revert[i['role']]
-    for response in assistant.run(messages[1:], var_dict=vd):
-        print(response)
-    messages.extend(response)
-    for i in messages:
-        i['role'] = revert[i['role']]
-    # print(response)
+    messages_for_u.extend(response)
+    messages_for_a.append({
+        'role': 'user',
+        'content': '\n'.join(i['content'] for i in response)
+    })
+    for response in assistant.run(messages_for_a, var_dict=vd):
+        ...
+    messages_for_a.extend(response)
+    messages_for_u.append({
+        'role': 'user',
+        'content': '\n'.join(i['content'] for i in response)
+    })
 
 print(vd.get('lyrics', 'lyrics not generated'))
 #'''
